e21f29adfbd39926efde1ca88a82b24498d0d8a1
Kristijonas Zalys <kzalys@uber.com>
2019-07-07 00:29:59 +0300

    Set up heartbeat API endpoint

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Hacker Suite - Auth
 
+ ## WIP
+
+ ## Set up
+
+  * `go get github.com/unicsmcr/hs_auth`
+  * navigate to root directory
+  * `go run main.go`
+  * app should be accessible at localhost:8080
 
  ## License
  Hacker Suite - Auth is licensed under the MIT License.
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type heartbeatResponse struct {
+	Status string `json:"status"`
+	Code   int    `json:"code"`
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", heartbeat)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func heartbeat(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(heartbeatResponse{Status: "OK", Code: 200})
+}
