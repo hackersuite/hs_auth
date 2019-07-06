4bb7e1f8ed047f9d007b793991cf2fe198ac1dab
Kristijonas Zalys <kzalys@uber.com>
2019-07-07 00:39:29 +0300

    Add CI config

--- main.go
+++ main.go
@@ -22,3 +22,7 @@ func main() {
 func heartbeat(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(heartbeatResponse{Status: "OK", Code: 200})
 }
+
+func SampleFunc(a, b int) int {
+	return a + b
+}
