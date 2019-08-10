be2a8f1dc53ea9c6e129e0d9bd8a8e48ff0886f5
Kristijonas Zalys <kzalys@gmail.com>
2019-08-10 10:56:49 +0300

    Implement UserService

--- services/userService.go
+++ services/userService.go
@@ -3,24 +3,51 @@ package services
 import (
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
+
 	"github.com/unicsmcr/hs_auth/entities"
 	"github.com/unicsmcr/hs_auth/repositories"
 )
 
+// UserService is the service for interactinos with the user collection
 type UserService struct {
 	userRepository repositories.UserRepository
 }
 
+// NewUserService creates a new UserService
 func NewUserService(userRepository repositories.UserRepository) UserService {
 	return UserService{
 		userRepository: userRepository,
 	}
 }
 
+// GetUserWithEmailAndPassword fetches a user with given email and password
+func (s UserService) GetUserWithEmailAndPassword(ctx context.Context, email string, password string) (*entities.User, error) {
+	res := s.userRepository.FindOne(ctx, bson.M{
+		"email":    email,
+		"password": password,
+	})
+
+	err := res.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	var user entities.User
+
+	err = res.Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// GetUsers fetches all users
 func (s UserService) GetUsers(ctx context.Context) ([]entities.User, error) {
 	users := []entities.User{}
 
-	cur, err := s.userRepository.Find(ctx, entities.User{})
+	cur, err := s.userRepository.Find(ctx, bson.M{})
 	if err != nil {
 		return nil, err
 	}
