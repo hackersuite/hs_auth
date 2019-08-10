193847446fc63b67a40c17aa1c30c28a0a23cf82
Kristijonas Zalys <kzalys@gmail.com>
2019-08-10 10:56:34 +0300

    Implement UserRepository

--- repositories/userRepository.go
+++ repositories/userRepository.go
@@ -4,12 +4,14 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// UserRepository is the repository for user objects
 type UserRepository struct {
 	*mongo.Collection
 }
 
+// NewUserRepository creates a new UserRepository
 func NewUserRepository(db *mongo.Database) UserRepository {
 	return UserRepository{
-		Collection: db.Collection("user"),
+		Collection: db.Collection("users"),
 	}
 }
