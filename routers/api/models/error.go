0573ca3ca8ddcca39def01c0e667bc7459316dab
Kristijonas Zalys <kzalys@gmail.com>
2019-08-10 10:56:11 +0300

    Update error model

--- routers/api/models/error.go
+++ routers/api/models/error.go
@@ -1,6 +1,28 @@
 package models
 
-type Error struct {
-	Status int   `json:"status"`
-	Err    error `json:"error"`
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// APIError is a struct to store a standard API error
+type APIError struct {
+	Status int    `json:"status"`
+	Err    string `json:"error"`
+}
+
+func (e *APIError) Error() string {
+	return e.Err
+}
+
+// APIError creates an APIError with given status and error message
+func NewAPIError(status int, err string) APIError {
+	return APIError{
+		Status: status,
+		Err:    err,
+	}
+}
+
+// SendAPIError sends an error with given status and error message to the user
+func SendAPIError(ctx *gin.Context, status int, err string) {
+	ctx.JSON(status, NewAPIError(status, err))
 }
