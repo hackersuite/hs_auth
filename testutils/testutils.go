dde44dae70627fb000cac1c57f30df315c83b415
Kristijonas Zalys <kzalys@gmail.com>
2019-09-03 00:07:47 +0300

    Implement RouterGroupMatcher

--- testutils/testutils.go
+++ testutils/testutils.go
@@ -1,6 +1,10 @@
 package testutils
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
 
 // SetEnvVars sets given environment variables and provides a callback function to restore the variables to their initial values
 func SetEnvVars(vars map[string]string) (restoreVars func()) {
@@ -38,7 +42,7 @@ func SetEnvVars(vars map[string]string) (restoreVars func()) {
 	}
 }
 
-// SetEnvVars unsets given environment variables and provides a callback function to restore the variables to their initial values
+// UnsetVars unsets given environment variables and provides a callback function to restore the variables to their initial values
 func UnsetVars(vars ...string) (restoreVars func()) {
 	initialValues := map[string]string{}
 	for _, name := range vars {
@@ -61,4 +65,41 @@ func UnsetVars(vars ...string) (restoreVars func()) {
 			}
 		}
 	}
+}
+
+// RouterGroupMatcher matches gin router groups with given path
+type RouterGroupMatcher struct {
+	// Path is the base path of the router groups to match
+	Path string
+}
+
+// Matches implements the gomock.Matcher interface
+func (r RouterGroupMatcher) Matches(x interface{}) bool {
+	val := reflect.ValueOf(x)
+	if reflect.Zero(reflect.TypeOf(x)) == val {
+		return false
+	}
+	// fetching reference to BasePath
+	basePathMethod := val.MethodByName("BasePath")
+	if reflect.Zero(reflect.TypeOf(basePathMethod)) == basePathMethod {
+		return false
+	}
+	// calling BasePath
+	values := basePathMethod.Call([]reflect.Value{})
+	if len(values) != 1 {
+		return false
+	}
+	// checking if value is string
+	if !values[0].Type().AssignableTo(reflect.TypeOf("")) {
+		return false
+	}
+
+	if values[0].String() != r.Path {
+		return false
+	}
+	return true
+}
+
+func (r RouterGroupMatcher) String() string {
+	return fmt.Sprintf("router group's base path is %s", r.Path)
 }
