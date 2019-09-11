e298416536b04ce7b4bb36cfa4604643e1ccfc63
Kristijonas Zalys <kzalys@gmail.com>
2019-09-11 23:12:49 +0300

    Integrate hashing methods into UserService

--- services/userService.go
+++ services/userService.go
@@ -3,6 +3,8 @@ package services
 import (
 	"context"
 
+	"github.com/unicsmcr/hs_auth/utils/auth"
+
 	authlevels "github.com/unicsmcr/hs_auth/utils/auth/common"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -62,12 +64,17 @@ func (s *userService) GetUserWithID(ctx context.Context, id string) (*entities.U
 
 // GetUserWithEmailAndPassword fetches a user with given email and password
 func (s *userService) GetUserWithEmailAndPassword(ctx context.Context, email string, password string) (*entities.User, error) {
+	hashedPassword, err := auth.GetHashForPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
 	res := s.userRepository.FindOne(ctx, bson.M{
 		"email":    email,
-		"password": password,
+		"password": hashedPassword,
 	})
 
-	err := res.Err()
+	err = res.Err()
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			return nil, ErrNotFound
@@ -108,6 +115,8 @@ func (s *userService) GetUsers(ctx context.Context) ([]entities.User, error) {
 	return users, nil
 }
 
+// UpdateUserWithID updates a user with the given id with the values in fieldsToUpdate
+// where the key must match the json name for the field to update in the user model
 func (s *userService) UpdateUserWithID(ctx context.Context, id string, fieldsToUpdate map[string]interface{}) error {
 	mongoID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
@@ -123,11 +132,24 @@ func (s *userService) UpdateUserWithID(ctx context.Context, id string, fieldsToU
 	return err
 }
 
+// CreateUser creates a new user with a hashed password
 func (s *userService) CreateUser(ctx context.Context, name, email, password string, authLevel authlevels.AuthLevel) (*entities.User, error) {
-	return &entities.User{
+	hashedPassword, err := auth.GetHashForPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &entities.User{
 		Name:      name,
 		Email:     email,
-		Password:  password,
+		Password:  hashedPassword,
 		AuthLevel: authLevel,
-	}, nil
+	}
+
+	_, err = s.userRepository.InsertOne(ctx, *user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
