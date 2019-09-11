3146e8412f182eb1b5c2bae79c11bb7d367e05e4
Kristijonas Zalys <kzalys@uber.com>
2019-09-11 23:06:19 +0300

    Implement Auth methods for password hashing

--- utils/auth/auth_test.go
+++ utils/auth/auth_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/unicsmcr/hs_auth/utils/auth/common"
 
 	"github.com/dgrijalva/jwt-go"
@@ -84,4 +86,30 @@ func Test_NewEmailToken__should_generate_a_random_token(t *testing.T) {
 		assert.False(t, exists)
 		tokens[token] = true
 	}
+}
+
+func Test_GetHashForPassword__should_return_expected_hash(t *testing.T) {
+	hash, err := GetHashForPassword("test password")
+	assert.NoError(t, err)
+
+	err = bcrypt.CompareHashAndPassword([]byte(hash), []byte("test password"))
+	assert.NoError(t, err)
+}
+
+func Test_CompareHashAndPassword__should_return_nil_for_valid_hash_and_password_combination(t *testing.T) {
+	password := "test password"
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	assert.NoError(t, err)
+
+	assert.NoError(t, CompareHashAndPassword(string(hash), password))
+}
+
+func Test_CompareHashAndPassword__should_return_nil_for_invalid_hash_and_password_combination(t *testing.T) {
+	password := "test password"
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	assert.NoError(t, err)
+
+	password += "invalid"
+
+	assert.Error(t, CompareHashAndPassword(string(hash), password))
 }
