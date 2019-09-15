1699b588f44a0421e62102e0df1d36a06bf71b9f
Kristijonas Zalys <kzalys@gmail.com>
2019-09-15 14:05:40 +0300

    Add unique name index to TeamRepository

--- repositories/teamRepository_test.go
+++ repositories/teamRepository_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/unicsmcr/hs_auth/testutils"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -14,8 +15,31 @@ import (
 func Test_NewTeamRepository__should_return_teams_mongo_collection(t *testing.T) {
 	db := testutils.ConnectToIntegrationTestDB(t)
 
-	tRepo := NewTeamRepository(db)
+	tRepo, err := NewTeamRepository(db)
+	assert.NoError(t, err)
 
 	assert.Equal(t, "teams", tRepo.Name())
+	db.Collection("teams").Drop(context.Background())
+}
+
+func Test_NewTeamRepository__create_required_number_of_indexes(t *testing.T) {
+	db := testutils.ConnectToIntegrationTestDB(t)
+
+	_, err := NewTeamRepository(db)
+	assert.NoError(t, err)
+
+	cur, err := db.Collection("teams").Indexes().List(context.Background())
+	assert.NoError(t, err)
+	defer cur.Close(context.Background())
+
+	var noOfIndexes int
+	for cur.Next(context.Background()) {
+		var index mongo.IndexModel
+		err = cur.Decode(&index)
+		assert.NoError(t, err)
+		noOfIndexes++
+	}
+
+	assert.Equal(t, 2, noOfIndexes)
 	db.Collection("teams").Drop(context.Background())
 }
