53b65c6555f7804dccf14bcb93f4650461e20351
Kristijonas Zalys <kzalys@uber.com>
2019-09-15 14:32:35 +0300

    Implement CreateTeam and DeleteTeamWithID

--- services/teamService_test.go
+++ services/teamService_test.go
@@ -82,6 +82,35 @@ func Test_GetTeamWithID__should_return_correct_team(t *testing.T) {
 	assert.Equal(t, testTeam, *team)
 }
 
+func Test_CreateTeam__should_create_required_team(t *testing.T) {
+	tRepo, tService := setupTeamTest(t)
+	defer tRepo.Drop(context.Background())
+
+	createdTeam, err := tService.CreateTeam(context.Background(), "test team 1", "2134abd12312312321312313")
+	assert.NoError(t, err)
+
+	teamOnDB, err := tService.GetTeamWithID(context.Background(), createdTeam.ID.Hex())
+	assert.NoError(t, err)
+
+	assert.Equal(t, teamOnDB, createdTeam)
+}
+
+func Test_DeleteTeamWithID__should_delete_correct_team(t *testing.T) {
+	tRepo, tService := setupTeamTest(t)
+	defer tRepo.Drop(context.Background())
+
+	testTeam, err := tService.CreateTeam(context.Background(), "test team 1", primitive.NewObjectID().Hex())
+	assert.NoError(t, err)
+
+	err = tService.DeleteTeamWithID(context.Background(), testTeam.ID.Hex())
+	assert.NoError(t, err)
+
+	_, err = tService.GetTeamWithID(context.Background(), testTeam.ID.Hex())
+	assert.Error(t, err)
+
+	assert.Equal(t, ErrNotFound, err)
+}
+
 func Test_GetTeamWithID__should_return_error(t *testing.T) {
 	tests := []errTeamTestCase{
 		{
@@ -89,7 +118,7 @@ func Test_GetTeamWithID__should_return_error(t *testing.T) {
 			wantErr: ErrInvalidID,
 		},
 		{
-			name:    "when user with given id doesn't exist",
+			name:    "when team with given id doesn't exist",
 			id:      "2134abd12312312321312313",
 			wantErr: ErrNotFound,
 		},
@@ -104,6 +133,30 @@ func Test_GetTeamWithID__should_return_error(t *testing.T) {
 			}
 
 			_, err := tService.GetTeamWithID(context.Background(), tt.id)
+			assert.Error(t, err)
+
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
+
+func Test_DeleteTeamWithID__should_return_error(t *testing.T) {
+	tests := []errTeamTestCase{
+		{
+			name:    "when given id is invalid",
+			wantErr: ErrInvalidID,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tRepo, tService := setupTeamTest(t)
+			defer tRepo.Drop(context.Background())
+			if tt.prep != nil {
+				tt.prep(t, tRepo)
+			}
+
+			err := tService.DeleteTeamWithID(context.Background(), tt.id)
 			assert.Error(t, err)
 
 			assert.Equal(t, tt.wantErr, err)
