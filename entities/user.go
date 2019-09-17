4ec8fca86fff595a3aadd04e87a2d173bc832d46
Kristijonas Zalys <kzalys@gmail.com>
2019-09-17 19:38:24 +0300

    Omit password when marshalling user object

--- entities/user.go
+++ entities/user.go
@@ -10,7 +10,7 @@ type User struct {
 	ID            primitive.ObjectID `json:"_id" bson:"_id"`
 	Name          string             `json:"name" bson:"name" validate:"required"`
 	Email         string             `json:"email" bson:"email" validate:"required,email"`
-	Password      string             `json:"password" bson:"password" validate:"required,min=6,max=160"`
+	Password      string             `json:"-" bson:"password" validate:"required,min=6,max=160"`
 	EmailVerified bool               `json:"email_verified,omitempty" bson:"email_verified,omitempty"`
 	EmailToken    string             `json:"email_token,omitempty" bson:"email_token,omitempty"`
 	AuthLevel     common.AuthLevel   `json:"auth_level,omitempty" bson:"auth_level,omitempty" validate:"min=0,max=3"`
