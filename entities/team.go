1d9a8e26e463ed160a934a12bfa769195feae495
Kristijonas Zalys <kzalys@uber.com>
2019-11-12 22:27:56 +0200

    Add table num to team entity

--- entities/team.go
+++ entities/team.go
@@ -8,5 +8,6 @@ import (
 type Team struct {
 	ID      primitive.ObjectID `json:"_id" bson:"_id"`
 	Name    string             `json:"name"  bson:"name" validate:"required"`
+	TableNo int                `json:"table_no" bson:"table_no"`
 	Creator primitive.ObjectID `json:"creator" bson:"creator" validate:"required"`
 }
