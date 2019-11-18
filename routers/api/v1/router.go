000c71d50a913b58168c0a2fb1150bd5c698a31a
Kristijonas Zalys <kzalys@gmail.com>
2019-11-18 22:56:54 +0200

    Cleanup

--- routers/api/v1/router.go
+++ routers/api/v1/router.go
@@ -1,28 +1,39 @@
 package v1
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/unicsmcr/hs_auth/config"
 	"github.com/unicsmcr/hs_auth/environment"
 	"github.com/unicsmcr/hs_auth/routers/api/models"
 	"github.com/unicsmcr/hs_auth/services"
+	"github.com/unicsmcr/hs_auth/utils/auth"
 	authlevels "github.com/unicsmcr/hs_auth/utils/auth/common"
 	"go.uber.org/zap"
 )
 
 const authHeaderName = "Authorization"
 
+func jwtProvider(ctx *gin.Context) string {
+	return ctx.GetHeader(authHeaderName)
+}
+
+func invalidJWTHandler(ctx *gin.Context) {
+	models.SendAPIError(ctx, http.StatusUnauthorized, "you are not authorized to use this endpoint")
+	ctx.Abort()
+	return
+}
+
 // APIV1Router is the router for v1 of the API
 type APIV1Router interface {
 	models.Router
 	GetUsers(*gin.Context)
 	Login(*gin.Context)
-	Verify(*gin.Context)
 	GetMe(*gin.Context)
 	PutMe(*gin.Context)
 	Register(*gin.Context)
 	VerifyEmail(*gin.Context)
-	AuthLevelVerifierFactory(level authlevels.AuthLevel) func(*gin.Context)
 	GetTeams(*gin.Context)
 	CreateTeam(*gin.Context)
 	LeaveTeam(*gin.Context)
@@ -36,14 +47,14 @@ type apiV1Router struct {
 	models.BaseRouter
 	logger       *zap.Logger
 	cfg          *config.AppConfig
-	userService  services.UserService
-	emailService services.EmailService
+	userService  services.UserServiceV2
+	emailService services.EmailServiceV2
 	teamService  services.TeamService
 	env          *environment.Env
 }
 
 // NewAPIV1Router creates a APIV1Router
-func NewAPIV1Router(logger *zap.Logger, cfg *config.AppConfig, userService services.UserService, emailService services.EmailService, teamService services.TeamService, env *environment.Env) APIV1Router {
+func NewAPIV1Router(logger *zap.Logger, cfg *config.AppConfig, env *environment.Env, userService services.UserServiceV2, emailService services.EmailServiceV2, teamService services.TeamService) APIV1Router {
 	return &apiV1Router{
 		logger:       logger,
 		cfg:          cfg,
@@ -58,15 +69,14 @@ func NewAPIV1Router(logger *zap.Logger, cfg *config.AppConfig, userService servi
 func (r apiV1Router) RegisterRoutes(routerGroup *gin.RouterGroup) {
 	routerGroup.GET("/", r.Heartbeat)
 
-	isAtLeastApplicant := r.AuthLevelVerifierFactory(authlevels.Applicant)
-	isAtLeastOrganizer := r.AuthLevelVerifierFactory(authlevels.Organizer)
+	isAtLeastApplicant := auth.AuthLevelVerifierFactory(authlevels.Applicant, jwtProvider, []byte(r.env.Get((environment.JWTSecret))), invalidJWTHandler)
+	isAtLeastOrganizer := auth.AuthLevelVerifierFactory(authlevels.Organizer, jwtProvider, []byte(r.env.Get((environment.JWTSecret))), invalidJWTHandler)
 
 	usersGroup := routerGroup.Group("/users")
 	usersGroup.GET("/", isAtLeastOrganizer, r.GetUsers)
 	usersGroup.POST("/", r.Register)
 	usersGroup.POST("/login", r.Login)
 	usersGroup.POST("/email/verify", r.VerifyEmail)
-	usersGroup.GET("/verify", isAtLeastApplicant, r.Verify)
 	usersGroup.GET("/me", isAtLeastApplicant, r.GetMe)
 	usersGroup.PUT("/me", isAtLeastApplicant, r.PutMe)
 	usersGroup.GET("/password/reset", r.GetPasswordResetEmail)
