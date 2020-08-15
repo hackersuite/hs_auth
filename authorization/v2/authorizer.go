fa8362e271e78d7dd05f24d7fb394cc00ffb94c7
Sean Parker <sean.parker067@gmail.com>
2020-08-15 07:55:10 +0100

    feat: add filter GetAuthorizedResources to only return authorized resources

--- authorization/v2/authorizer.go
+++ authorization/v2/authorizer.go
@@ -87,8 +87,15 @@ func (a *authorizer) GetAuthorizedResources(token string, urisToCheck []UniformR
 		return nil, errors.Wrap(ErrInvalidToken, err.Error())
 	}
 
-	// TODO: implement filtering for resources the token does not have access to
-	return urisToCheck, nil
+	// filtering for resources the token has access to
+	var allowedResources []UniformResourceIdentifier
+	for _, resource := range claims.AllowedResources {
+		if resource.isSubsetOfAtLeastOne(urisToCheck) {
+			allowedResources = append(allowedResources, resource)
+		}
+	}
+
+	return allowedResources, nil
 }
 
 func (a *authorizer) WithAuthMiddleware(router resources.RouterResource, operationHandler gin.HandlerFunc) gin.HandlerFunc {
