fd0d79d11a45950df2a82a48b5dd7ed28aafc07d
Sean Parker <sean.parker067@gmail.com>
2020-08-24 20:17:30 +0100

    chore: cleanup service token creation

--- routers/api/v2/tokens.go
+++ routers/api/v2/tokens.go
@@ -6,55 +6,56 @@ import (
 	"github.com/unicsmcr/hs_auth/routers/api/models"
 	"go.uber.org/zap"
 	"net/http"
-	"strconv"
 	"strings"
 )
 
-func (r *apiV2Router) ServiceToken(ctx *gin.Context) {
-	owner := ctx.PostForm("owner")
-	allowedURIs := ctx.PostForm("allowedURIs")
-	expiresAt := ctx.PostForm("expiresAt")
-
-	if len(owner) == 0 {
-		r.logger.Debug("service name was not provided in request")
-		models.SendAPIError(ctx, http.StatusBadRequest, "service name for the token must be provided")
+// POST: /api/v2/tokens/service
+// x-www-form-urlencoded
+// Request:	 owner string
+//			 allowedURIs string
+//			 expiresAt int64
+// Response: token string
+// Headers:  Authorization <- token
+func (r *apiV2Router) CreateServiceToken(ctx *gin.Context) {
+	var req struct {
+		Owner       string `form:"owner"`
+		AllowedURIs string `form:"allowedURIs"`
+		ExpiresAt   int64  `form:"expiresAt"`
+	}
+	err := ctx.Bind(&req)
+	if err != nil {
+		r.logger.Debug(err.Error())
+		models.SendAPIError(ctx, http.StatusBadRequest, "failed to parse request")
 		return
 	}
 
-	if len(allowedURIs) == 0 {
+	if len(req.Owner) == 0 {
+		r.logger.Debug("token owner was not provided in request")
+		models.SendAPIError(ctx, http.StatusBadRequest, "token owner must be provided")
+		return
+	}
+
+	if len(req.AllowedURIs) == 0 {
 		r.logger.Debug("no allowedURIs were provided in request")
 		models.SendAPIError(ctx, http.StatusBadRequest, "at least one allowedURI must be provided")
 		return
 	}
 
-	// expiresAt is optional in the form, the default value is -1, i.e. never expires
-	if len(expiresAt) == 0 {
-		expiresAt = "-1"
-	}
-
-	uriList := strings.Split(allowedURIs, ",")
-	var parsedURIs []v2.UniformResourceIdentifier
-	for _, uriString := range uriList {
-		uri, err := v2.NewURIFromString(uriString)
+	uriList := strings.Split(req.AllowedURIs, ",")
+	parsedURIs := make([]v2.UniformResourceIdentifier, len(uriList))
+	for i, uriString := range uriList {
+		err = parsedURIs[i].UnmarshalJSON([]byte(uriString))
 		if err != nil {
 			r.logger.Error("provided URI could not be parsed", zap.Error(err))
 			models.SendAPIError(ctx, http.StatusBadRequest, "invalid URI string in allowedURIs")
 			return
 		}
-		parsedURIs = append(parsedURIs, uri)
 	}
 
-	parsedExpiresAt, err := strconv.ParseInt(expiresAt, 10, 64)
-	if err != nil {
-		r.logger.Error("expiresAt time could not be parsed", zap.Error(err))
-		models.SendAPIError(ctx, http.StatusBadRequest, "expiresAt must be an integer")
-		return
-	}
-
-	token, err := r.authorizer.CreateServiceToken(owner, parsedURIs, parsedExpiresAt)
+	token, err := r.authorizer.CreateServiceToken(req.Owner, parsedURIs, req.ExpiresAt)
 	if err != nil {
 		r.logger.Error("could not create JWT", zap.Error(err))
-		models.SendAPIError(ctx, http.StatusInternalServerError, "there was a problem with creating service token")
+		models.SendAPIError(ctx, http.StatusInternalServerError, "something went wrong")
 		return
 	}
 
